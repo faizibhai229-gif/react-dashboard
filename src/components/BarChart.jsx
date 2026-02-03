c1cd33d8a61f962850a3bf97f205043fd03833bb
faizibhai229-gif <faizibhai229@gmail.com>
2026-02-03 19:18:44 +0500

    Add src/components/BarChart.jsx

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const CustomBarChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value" fill="#8884d8" />
+        <Bar dataKey="target" fill="#82ca9d" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomBarChart;
