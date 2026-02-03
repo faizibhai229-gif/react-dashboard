0abe781849a3bd0eadd87b7a6ba629c995c2e3d7
faizibhai229-gif <faizibhai229@gmail.com>
2026-02-03 19:18:46 +0500

    Add src/components/LineChart.jsx

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const CustomLineChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="users" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="growth" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomLineChart;
